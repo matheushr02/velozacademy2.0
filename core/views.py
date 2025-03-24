52f49728087c735a35765a19fa3f779711548857
maletros <meteos7006@gmail.com>
2025-03-24 18:01:47 -0300

    melhorias

--- core/views.py
+++ core/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from cursos.models import Curso
 from projetos.models import Projeto
+import random
 
 def home(request):
-    cursos = Curso.objects.all()[:3]  # Get latest 3 courses
-    projetos = Projeto.objects.all()[:3]  # Get latest 3 projects
+    # Obter todos os cursos e projetos
+    todos_cursos = list(Curso.objects.all())
+    todos_projetos = list(Projeto.objects.all())
+    
+    # Selecionar 3 cursos aleatórios para exibir como "cursos em destaque" na página inicial
+    # Se houver menos de 3 cursos, usa todos os disponíveis
+    cursos_destaque = random.sample(todos_cursos, min(3, len(todos_cursos)))
+    
+    # Selecionar 3 projetos aleatórios para exibir como "projetos populares" na página inicial
+    # Se houver menos de 3 projetos, usa todos os disponíveis
+    projetos_populares = random.sample(todos_projetos, min(3, len(todos_projetos)))
     
     context = {
-        'cursos': cursos,
-        'projetos': projetos,
+        'cursos_destaque': cursos_destaque,
+        'projetos_populares': projetos_populares,
     }
     return render(request, 'core/home.html', context)
